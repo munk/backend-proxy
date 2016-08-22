d7240b43b79a9e8cd9cb7b4f596565229e9dad30
john.downs <john.downs@yodle.com>
2016-08-22 12:18:27 -0400

    YO-30726 create stub project for http backend proxy demo

--- app.spec.js
+++ app.spec.js
@@ -1,3 +1,6 @@
+var HttpBackend = require('http-backend-proxy');
+var proxy = new HttpBackend(browser);
+
 describe('the test...', function() {
 
     beforeEach(function() {
@@ -5,6 +8,8 @@ describe('the test...', function() {
     });
 
     it('passes', function() {
+        proxy.when('GET', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular.jxs').respond(200);
+
         expect(true).toEqual(true);
     });
 });
